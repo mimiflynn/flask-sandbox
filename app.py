02343f37d6a11358c9598ad12a7b542239a44f52
Mimi Flynn <mimiflynn@gmail.com>
2017-09-03 19:39:18 -0400

    url parameters

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, send_from_directory
-
+from flask import Flask, render_template, request, send_from_directory, url_for
 
 app = Flask(__name__)
+
+class Dimensions(object):
+    height = 0
+    width = 0
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
 
 
 @app.route('/')
@@ -12,6 +19,26 @@ def index():
 @app.route('/styles/<path:path>')
 def styles(path):
     return send_from_directory('styles', path)
+
+
+@app.route('/timelapse')
+def timelapse():
+    height = request.args.get('height')
+    width = request.args.get('width')
+    return render_template('params.html', dimensions=Dimensions(height, width))
+
+
+@app.route('/login')
+def login(): pass
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='John Doe')
 
 
 if __name__ == '__main__':
